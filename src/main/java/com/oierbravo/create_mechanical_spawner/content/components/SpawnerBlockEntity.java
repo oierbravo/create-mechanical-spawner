448debe11a52d3b657b7c554cfc360727a90d53d
snakemasterepic <snakework1997@gmail.com>
2025-01-02 16:20:02 -0800

    Update SpawnerBlockEntity.java
    Fixed case with loot collector as well

--- src/main/java/com/oierbravo/create_mechanical_spawner/content/components/SpawnerBlockEntity.java
+++ src/main/java/com/oierbravo/create_mechanical_spawner/content/components/SpawnerBlockEntity.java
@@ -317,11 +317,11 @@ public class SpawnerBlockEntity extends KineticBlockEntity  implements DynamicCy
 
         fluidTank.drain(getRecipe().get().getFluidAmount(), IFluidHandler.FluidAction.EXECUTE);
 
-        if(isSpawnPosBlockLootCollector()){
-            fillCollector(level,getRecipe().get(), getSpawnPos() );
-        } else {
-            Optional<SpawnerRecipe> recipe = getRecipe();
-            if (recipe.isPresent()) {
+        Optional<SpawnerRecipe> recipe = getRecipe();
+        if (recipe.isPresent()) {
+            if(isSpawnPosBlockLootCollector()){
+                fillCollector(level,recipe.get(), getSpawnPos() );
+            } else {
                 LivingEntityHelper.spawnLivingEntity(level,recipe.get().getMob(), getSpawnPos() );
             }
         }
