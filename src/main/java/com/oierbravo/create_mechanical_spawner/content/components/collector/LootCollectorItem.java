3781bb2f49111d43d99a7a1dd87f2d75ffaaf3e4
Oier Bravo Urtasun <oierbravo@gmail.com>
2025-05-23 23:57:04 +0200

    Enchantable loot collector #3

--- src/main/java/com/oierbravo/create_mechanical_spawner/content/components/collector/LootCollectorItem.java
+++ src/main/java/com/oierbravo/create_mechanical_spawner/content/components/collector/LootCollectorItem.java
@@ -4,13 +4,34 @@ import net.minecraft.core.Holder;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
 import net.minecraft.world.level.block.Block;
+
+import java.util.Set;
 
 public class LootCollectorItem extends BlockItem {
 
     public LootCollectorItem(Block block, Properties properties)  {
         super(block, properties);
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return 70;
+    }
+    @Override
+    public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
+        ItemEnchantments itemEnchantments = EnchantmentHelper.getEnchantmentsForCrafting(book);
+        Set<Holder<Enchantment>> enchantments = itemEnchantments.keySet();
+        if(enchantments.size() > 1)
+            return false;
+        for(Holder<Enchantment> enchantment : enchantments ){
+            if(enchantment.is(Enchantments.LOOTING))
+                return true;
+        }
+        return false;
     }
 
     @Override
@@ -22,9 +43,5 @@ public class LootCollectorItem extends BlockItem {
         if (enchantment.is(Enchantments.LOOTING))
             return true;
         return super.supportsEnchantment(stack, enchantment);
-    }
-    @Override
-    public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
-        return super.isBookEnchantable(stack, book);
     }
 }
